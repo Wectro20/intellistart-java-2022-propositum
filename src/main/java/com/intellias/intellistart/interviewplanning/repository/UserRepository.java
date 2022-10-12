6647c0b589770b9544f3278b0127273aa1e0a839
Vova <vovamitsura@gmail.com>
2022-10-12 10:03:53 +0300

    added findByEmail method

--- src/main/java/com/intellias/intellistart/interviewplanning/repository/UserRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.intellias.intellistart.interviewplanning.repository;
 
 import com.intellias.intellistart.interviewplanning.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  Optional<User> findByEmail(String candidateEmail);
 }

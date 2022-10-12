4b3e57d0c9c36f73448b10bfe005fd9a310c2723
Vova <vovamitsura@gmail.com>
2022-10-12 10:02:27 +0300

    added TimeSLotStatus for time slots and Views for request and response

---
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/TimeSLotStatus.java
@@ -0,0 +1,12 @@
+package com.intellias.intellistart.interviewplanning.model;
+
+/**
+ * Status of time slot.
+ */
+public enum TimeSLotStatus {
+  NEW,
+  CHANGED,
+  PRE_BOOKED,
+  BOOKED,
+  DELETED
+}

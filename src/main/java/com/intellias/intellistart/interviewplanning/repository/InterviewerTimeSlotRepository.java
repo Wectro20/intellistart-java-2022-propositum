b4f486dc3b5a9b8f52f884fae72131912d778485
Vova <vovamitsura@gmail.com>
2022-10-12 11:19:31 +0300

    implemented InterviewerTimeSlotController

--- src/main/java/com/intellias/intellistart/interviewplanning/repository/InterviewerTimeSlotRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repository/InterviewerTimeSlotRepository.java
@@ -1,6 +1,8 @@
 package com.intellias.intellistart.interviewplanning.repository;
 
+import com.intellias.intellistart.interviewplanning.model.User;
 import com.intellias.intellistart.interviewplanning.model.slot.InterviewerTimeSlot;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Repository;
  * Its repository for InterviewTimeSlot entity.
  */
 @Repository
-public interface InterviewerTimeSlotRepository extends JpaRepository<InterviewerTimeSlot, Long> {
+public interface InterviewerTimeSlotRepository extends JpaRepository<InterviewerTimeSlot, Integer> {
 
+  Optional<InterviewerTimeSlot> findByUserAndWeekNum(User user, int weekNum);
 }

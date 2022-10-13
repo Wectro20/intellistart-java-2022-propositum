7d1b651acb9c90f61e500f81af3e822093763128
Sviat2001 <cvcatuy@gmail.com>
2022-10-13 21:12:11 +0300

    [task6] Implement endpoint to update Interviewer Time Slot beta

--- src/main/java/com/intellias/intellistart/interviewplanning/repository/InterviewerTimeSlotRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/repository/InterviewerTimeSlotRepository.java
@@ -3,6 +3,7 @@ package com.intellias.intellistart.interviewplanning.repository;
 import com.intellias.intellistart.interviewplanning.model.User;
 import com.intellias.intellistart.interviewplanning.model.slot.InterviewerTimeSlot;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

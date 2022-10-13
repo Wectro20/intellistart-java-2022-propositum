7c0e513443b0b172eadf1e1cb4cf7f42fbaff7b4
Vova <vovamitsura@gmail.com>
2022-10-13 13:58:33 +0300

    added tests

--- src/test/java/com/intellias/intellistart/interviewplanning/CandidateControllerTest.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/CandidateControllerTest.java
@@ -1,51 +1,212 @@
 package com.intellias.intellistart.interviewplanning;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
 import com.intellias.intellistart.interviewplanning.controller.CandidateController;
+import com.intellias.intellistart.interviewplanning.exceptions.InvalidDayOfWeekException;
+import com.intellias.intellistart.interviewplanning.exceptions.InvalidTimeSlotBoundariesException;
+import com.intellias.intellistart.interviewplanning.exceptions.SlotIsOverlappingException;
+import com.intellias.intellistart.interviewplanning.exceptions.UserNotFoundException;
+import com.intellias.intellistart.interviewplanning.model.TimeSlotStatus;
+import com.intellias.intellistart.interviewplanning.model.User;
+import com.intellias.intellistart.interviewplanning.model.User.UserRole;
 import com.intellias.intellistart.interviewplanning.model.slot.CandidateTimeSlot;
 import com.intellias.intellistart.interviewplanning.service.CandidateTimeSlotService;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest(controllers = CandidateController.class)
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 class CandidateControllerTest {
 
-  @Autowired
-  private CandidateController candidateController;
-  @Autowired
+  @MockBean
   private CandidateTimeSlotService candidateTimeSlotService;
 
-  /*
-  @Test
-  void createCandidateTimeSlot() {
-    CandidateTimeSlot slot = candidateTimeSlotService.createSlot("user@gmail.com",
-        LocalDate.of(2022, 11, 10),
-        LocalTime.of(8, 30),
-        LocalTime.of(10, 0));
+  @Autowired
+  private MockMvc mockMvc;
 
-    ResponseEntity<CandidateTimeSlot> slot1 = candidateController.createSlot(slot);
+  private static final String userEmail = "user@gmail.com";
 
-    assertNull(slot1);
+  private static final User user = new User(1L, userEmail, UserRole.CANDIDATE);
+
+  private static final String url = "/candidates/current/slots";
+
+  private static CandidateTimeSlot candidateTimeSlot = CandidateTimeSlot.builder()
+      .date(LocalDate.of(2022, 11, 10))
+      .id(2L)
+      .from(LocalTime.of(9, 0))
+      .to(LocalTime.of(17, 0))
+      .slotStatus(TimeSlotStatus.NEW)
+      .user(user)
+      .build();
+
+  private static final String RIGHT_REQUEST = "{\n"
+      + "\"from\": \"09:00\",\n"
+      + "\"to\": \"17:00\",\n"
+      + "\"date\": \"2022-11-10\"\n"
+      + "}";
+
+  private static final String BAD_DAY_OF_WEEK_REQUEST = "{\n"
+      + "\"from\": \"09:00\",\n"
+      + "\"to\": \"17:00\",\n"
+      + "\"date\": \"2022-11-12\"\n"
+      + "}";
+
+  private static final String BAD_FROM_BOUNDARIES_REQUEST = "{\n"
+      + "\"from\": \"09:02\",\n"
+      + "\"to\": \"17:00\",\n"
+      + "\"date\": \"2022-11-10\"\n"
+      + "}";
+
+  private static final String BAD_FROM_TO_REQUEST = "{\n"
+      + "\"from\": \"09:00\",\n"
+      + "\"to\": \"10:00\",\n"
+      + "\"date\": \"2022-11-10\"\n"
+      + "}";
+
+  private static final String RIGHT_RESPONSE = "{"
+      + "\"id\":2,"
+      + "\"from\":\"09:00\","
+      + "\"to\":\"17:00\","
+      + "\"date\":\"2022-11-10\""
+      + "}";
+
+  private static final String BAD_DAY_OF_WEEK_RESPONSE = "{"
+      + "\"errorCode\":\"invalid_day_of_week\","
+      + "\"errorMessage\":\"Possible values: [Mon, Tue, Wed, Thu, Fri, Sat, Sun]"
+      + "\"}";
+
+  private static final String BAD_FROM_BOUNDARIES_EXCEPTION = "{"
+      + "\"errorCode\":\"invalid_boundaries\","
+      + "\"errorMessage\":\"09:02; 17:00\""
+      + "}";
+
+  private static final String BAD_FROM_TO_BOUNDARIES_EXCEPTION = "{"
+      + "\"errorCode\":\"invalid_boundaries\","
+      + "\"errorMessage\":\"09:00; 10:00\""
+      + "}";
+
+  private static final String BAD_USER_NOT_FOUND_EXCEPTION = "{"
+      + "\"errorCode\":\"candidate_not_found\","
+      + "\"errorMessage\":\"user@gmail.com\""
+      + "}";
+
+  private static final String BAD_SLOT_IS_OVERLAPPING_EXCEPTION = "{"
+      + "\"errorCode\":\"slot_is_overlapping\","
+      + "\"errorMessage\":\"Slot is already exists, id of existing slot: 2\""
+      + "}";
+
+  @AfterEach
+  public void setUp() {
+    candidateTimeSlot = CandidateTimeSlot.builder()
+        .date(LocalDate.of(2022, 11, 10))
+        .id(2L)
+        .from(LocalTime.of(9, 0))
+        .to(LocalTime.of(17, 0))
+        .slotStatus(TimeSlotStatus.NEW)
+        .user(user)
+        .build();
   }
 
   @Test
-  void createCandidateTimeSlotAndStatusIsCreated() {
-    CandidateTimeSlot slot = candidateTimeSlotService.createSlot("user@gmail.com",
-        LocalDate.of(2022, 4, 12),
-        LocalTime.of(8, 30),
-        LocalTime.of(10, 0));
+  @WithMockUser(userEmail)
+  void createCandidateTimeSlot() throws Exception {
+    Mockito.when(candidateTimeSlotService.createSlot(userEmail, candidateTimeSlot.getDate(), candidateTimeSlot.getFrom(), candidateTimeSlot.getTo()))
+        .thenReturn(candidateTimeSlot);
 
-    ResponseEntity<CandidateTimeSlot> slot1 = candidateController.createSlot(slot);
+    this.mockMvc.perform(MockMvcRequestBuilders.post(url)
+            .content(RIGHT_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isCreated())
+        .andExpect(MockMvcResultMatchers.content().string(RIGHT_RESPONSE));
 
-    assertEquals(HttpStatus.CREATED, slot1.getStatusCode());
   }
-  */
+
+  @Test
+  @WithMockUser(userEmail)
+  void createCandidateTimeSlotWithInvalidDayOfWeekAndThrowException() throws Exception {
+    candidateTimeSlot.setDate(LocalDate.of(2022, 11, 12));
+    Mockito.when(candidateTimeSlotService.createSlot(userEmail, candidateTimeSlot.getDate(), candidateTimeSlot.getFrom(), candidateTimeSlot.getTo()))
+        .thenThrow(InvalidDayOfWeekException.class);
+
+    this.mockMvc.perform(MockMvcRequestBuilders.post(url)
+            .content(BAD_DAY_OF_WEEK_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andExpect(MockMvcResultMatchers.content().string(BAD_DAY_OF_WEEK_RESPONSE));
+  }
+
+  @Test
+  @WithMockUser(userEmail)
+  void createCandidateTimeSlotWithInvalidFromBoundariesAndThrowException() throws Exception {
+    candidateTimeSlot.setFrom(LocalTime.of(9, 2));
+    Mockito.when(candidateTimeSlotService.createSlot(userEmail, candidateTimeSlot.getDate(), candidateTimeSlot.getFrom(), candidateTimeSlot.getTo()))
+        .thenThrow(new InvalidTimeSlotBoundariesException(candidateTimeSlot.getFrom() + "; " + candidateTimeSlot.getTo()));
+
+    this.mockMvc.perform(MockMvcRequestBuilders.post(url)
+            .content(BAD_FROM_BOUNDARIES_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andExpect(MockMvcResultMatchers.content().string(BAD_FROM_BOUNDARIES_EXCEPTION));
+  }
+
+  @Test
+  @WithMockUser(userEmail)
+  void createCandidateTimeSlotWithIntervalLessThanMinAndThrowException() throws Exception {
+    candidateTimeSlot.setTo(LocalTime.of(10, 0));
+    Mockito.when(candidateTimeSlotService.createSlot(userEmail, candidateTimeSlot.getDate(), candidateTimeSlot.getFrom(), candidateTimeSlot.getTo()))
+        .thenThrow(new InvalidTimeSlotBoundariesException(candidateTimeSlot.getFrom() + "; " + candidateTimeSlot.getTo()));
+
+    this.mockMvc.perform(MockMvcRequestBuilders.post(url)
+            .content(BAD_FROM_TO_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andExpect(MockMvcResultMatchers.content().string(BAD_FROM_TO_BOUNDARIES_EXCEPTION));
+  }
+
+  @Test
+  @WithMockUser(userEmail)
+  void createCandidateTimeSlotInvalidUserAndThrowException() throws Exception {
+    Mockito.when(candidateTimeSlotService.createSlot(userEmail, candidateTimeSlot.getDate(), candidateTimeSlot.getFrom(), candidateTimeSlot.getTo()))
+        .thenThrow(new UserNotFoundException(userEmail));
+
+    this.mockMvc.perform(MockMvcRequestBuilders.post(url)
+            .content(RIGHT_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isNotFound())
+        .andExpect(MockMvcResultMatchers.content().string(BAD_USER_NOT_FOUND_EXCEPTION));
+  }
+
+  @Test
+  @WithMockUser(userEmail)
+  void createCandidateTimeSlotWhichOverlappingAndThrowException() throws Exception {
+    Mockito.when(candidateTimeSlotService.createSlot(userEmail, candidateTimeSlot.getDate(), candidateTimeSlot.getFrom(), candidateTimeSlot.getTo()))
+        .thenThrow(new SlotIsOverlappingException(candidateTimeSlot.getId()));
+
+    this.mockMvc.perform(MockMvcRequestBuilders.post(url)
+            .content(RIGHT_REQUEST)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isConflict())
+        .andExpect(MockMvcResultMatchers.content().string(BAD_SLOT_IS_OVERLAPPING_EXCEPTION));
+  }
+
 }
--- src/test/java/com/intellias/intellistart/interviewplanning/service/CandidateTimeSlotServiceTest.java
+++ src/test/java/com/intellias/intellistart/interviewplanning/service/CandidateTimeSlotServiceTest.java
@@ -2,35 +2,156 @@ package com.intellias.intellistart.interviewplanning.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.intellias.intellistart.interviewplanning.exceptions.InvalidDayOfWeekException;
+import com.intellias.intellistart.interviewplanning.exceptions.InvalidTimeSlotBoundariesException;
+import com.intellias.intellistart.interviewplanning.exceptions.SlotIsOverlappingException;
+import com.intellias.intellistart.interviewplanning.exceptions.UserNotFoundException;
+import com.intellias.intellistart.interviewplanning.model.TimeSlotStatus;
+import com.intellias.intellistart.interviewplanning.model.User;
+import com.intellias.intellistart.interviewplanning.model.User.UserRole;
+import com.intellias.intellistart.interviewplanning.model.slot.CandidateTimeSlot;
+import com.intellias.intellistart.interviewplanning.repository.CandidateTimeSlotRepository;
+import com.intellias.intellistart.interviewplanning.repository.UserRepository;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 class CandidateTimeSlotServiceTest {
 
-  @Autowired
+  @InjectMocks
   private CandidateTimeSlotService candidateTimeSlotService;
+  @MockBean
+  private UserRepository userRepository;
+  @MockBean
+  private CandidateTimeSlotRepository candidateTimeSlotRepository;
 
-  /*
+  private static final String userEmail = "user@gmail.com";
+  private static final User user = new User(1L, userEmail, UserRole.CANDIDATE);
+  private static final CandidateTimeSlot candidateTimeSlot = CandidateTimeSlot.builder()
+      .date(LocalDate.of(2022, 11, 10))
+      .from(LocalTime.of(9, 0))
+      .to(LocalTime.of(17, 0))
+      .slotStatus(TimeSlotStatus.NEW)
+      .user(user)
+      .build();
+
   @Test
   void createCandidateSlotWithParameters() {
+    Mockito.when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+
+    Mockito.when(candidateTimeSlotRepository.save(ArgumentMatchers.any()))
+        .thenReturn(candidateTimeSlot);
+
+    candidateTimeSlot.setId(1L);
+
     var slot = candidateTimeSlotService.createSlot(
-        "user@gmail.com",
-        LocalDate.of(2022, 4, 12),
+        userEmail,
+        LocalDate.of(2022, 11, 10),
         LocalTime.of(9, 0), // 09:00
         LocalTime.of(17, 0) // 17:00
     );
 
     assertNotNull(slot);
-    assertEquals(15L, slot.getUser().getId());
-    assertEquals(LocalDate.of(2022, 4, 12), slot.getDate());
+    assertEquals(1L, slot.getUser().getId());
+    assertEquals(LocalDate.of(2022, 11, 10), slot.getDate());
     assertEquals(LocalTime.of(9, 0), slot.getFrom());
     assertEquals(LocalTime.of(17, 0), slot.getTo());
+    assertEquals(TimeSlotStatus.NEW, slot.getSlotStatus());
   }
-   */
 
+  @Test
+  void createSlotWhenDayOfWeekIsWeekendAndThrowException() {
+    Mockito.when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+
+    InvalidDayOfWeekException exception = assertThrows(
+        InvalidDayOfWeekException.class, () -> candidateTimeSlotService.createSlot(userEmail,
+            LocalDate.of(2022, 11, 12),
+            LocalTime.of(17, 0), // 09:00
+            LocalTime.of(9, 0) // 17:00
+        ));
+
+    assertEquals("2022-11-12", exception.getMessage());
+  }
+
+  @Test
+  void createSlotWhenBoundariesIsNotRoundedAndThrowException() {
+    Mockito.when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+
+    InvalidTimeSlotBoundariesException exception = assertThrows(
+        InvalidTimeSlotBoundariesException.class,
+        () -> candidateTimeSlotService.createSlot(userEmail,
+            LocalDate.of(2022, 11, 10),
+            LocalTime.of(9, 2), // 09:00
+            LocalTime.of(17, 0) // 17:00
+        ));
+
+    assertEquals("09:02; 17:00", exception.getMessage());
+  }
+
+  @Test
+  void createSlotWhenFromIsAfterToAndThrowException() {
+    Mockito.when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+
+    InvalidTimeSlotBoundariesException exception = assertThrows(
+        InvalidTimeSlotBoundariesException.class,
+        () -> candidateTimeSlotService.createSlot(userEmail,
+            LocalDate.of(2022, 11, 10),
+            LocalTime.of(17, 0), // 09:00
+            LocalTime.of(9, 0) // 17:00
+        ));
+
+    assertEquals("17:00; 09:00", exception.getMessage());
+  }
+
+  @Test
+  void createSlotWithInvalidBoundariesAndThrowException() {
+    Mockito.when(userRepository.findByEmail(userEmail)).thenReturn(Optional.empty());
+
+    UserNotFoundException exception = assertThrows(
+        UserNotFoundException.class, () -> candidateTimeSlotService.createSlot(userEmail,
+            LocalDate.of(2022, 11, 10),
+            LocalTime.of(9, 0), // 09:00
+            LocalTime.of(17, 0) // 17:00
+        ));
+
+    assertEquals(userEmail, exception.getMessage());
+  }
+
+  @Test
+  void createSlotWhichOverlapsAndThrowException() {
+    CandidateTimeSlot candidateTimeSlot = CandidateTimeSlot.builder()
+        .id(2L)
+        .date(LocalDate.of(2022, 11, 10))
+        .from(LocalTime.of(9, 0))
+        .to(LocalTime.of(17, 0))
+        .slotStatus(TimeSlotStatus.NEW)
+        .user(user)
+        .build();
+
+    Mockito.when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+    Mockito.when(candidateTimeSlotRepository.findByUserId(user.getId()))
+        .thenReturn((List.of(candidateTimeSlot)));
+
+    SlotIsOverlappingException exception = assertThrows(
+        SlotIsOverlappingException.class, () -> candidateTimeSlotService.createSlot(userEmail,
+            LocalDate.of(2022, 11, 10),
+            LocalTime.of(9, 0), // 09:00
+            LocalTime.of(17, 0) // 17:00
+        ));
+
+    assertEquals("2", exception.getMessage().substring(exception.getMessage().length()-1));
+  }
 }

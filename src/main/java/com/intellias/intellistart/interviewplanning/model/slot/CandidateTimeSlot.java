0c7ed2282bb1a1fa85f554de95b7727d0960bbff
Wectro20 <shumskyy345@gmail.com>
2022-10-13 18:06:04 +0300

    Adding code for getting current/next number of week

--- src/main/java/com/intellias/intellistart/interviewplanning/model/slot/CandidateTimeSlot.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/slot/CandidateTimeSlot.java
@@ -3,11 +3,8 @@ package com.intellias.intellistart.interviewplanning.model.slot;
 import com.intellias.intellistart.interviewplanning.model.User;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,7 +23,9 @@ public class CandidateTimeSlot {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @Column(name = "\"from\"")
   private LocalTime from;
+  @Column(name = "\"to\"")
   private LocalTime to;
   private LocalDate date;
   @ManyToOne

57d5a407217d34403095ea55fd8f44ed3a709943
olena <olena.vitiuk123@gmail.com>
2022-10-21 11:58:44 +0200

    [task 25] fix merge conflicts

--- src/main/java/com/intellias/intellistart/interviewplanning/service/InterviewerTimeSlotService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/service/InterviewerTimeSlotService.java
@@ -166,7 +166,7 @@ public class InterviewerTimeSlotService {
     int nextWeekNum = weekService.getNextWeekNumber().getWeekNum();
 
     if (!weekNum.equals(nextWeekNum)) {
-      throw new WeekNumberNotAcceptableException(nextWeekNum);
+      throw new WeekNumberNotAcceptableException(Collections.singletonList(nextWeekNum));
     }
   }
 }

6fe3deca519901c030d4df105fe2065ed6f72176
Vova <vovamitsura@gmail.com>
2022-11-08 23:14:08 +0200

    Make JwtAuthenticationEntryPoint return error code and message

--- src/main/java/com/intellias/intellistart/interviewplanning/security/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/security/config/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,11 @@
 package com.intellias.intellistart.interviewplanning.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.intellias.intellistart.interviewplanning.exceptions.ApplicationExceptionHandler.ErrorResponse;
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -20,7 +22,15 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
    */
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response,
-      AuthenticationException authException) throws IOException, ServletException {
-    response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+      AuthenticationException authException) throws IOException {
+    ErrorResponse errorResponse = new ErrorResponse();
+    errorResponse.setErrorCode("not_authorized");
+    errorResponse.setErrorMessage("have not provide any credentials");
+
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    response.getOutputStream().println(objectMapper.writeValueAsString(errorResponse));
   }
 }
